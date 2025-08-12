aa4d175b82797f5362bc499b51554247abbd0c66
김기빈 <kibin3846@gmail.com>
2025-08-12 15:12:12 +0900

    feat: UX writing(분석 이미지 코멘트 업데이트)

--- src/components/AnalysisViewer.tsx
+++ src/components/AnalysisViewer.tsx
@@ -12,12 +12,23 @@ const AnalysisViewer = ({
   analysisCount,
   ruptureCount,
 }: AnalysisViewerProps) => {
+  const hasRupture = ruptureCount > 0;
+
   return (
     <div className="column">
       <NumberedList number={4} title="부작용 감지 이미지 첨부" />
       <div className="comment-box-image">
-        총 {analysisCount}장의 이미지 중 {ruptureCount}장에서 파열이
-        감지되었습니다.
+        {hasRupture ? (
+          <>
+            총 {analysisCount}장의 이미지 중 {ruptureCount}장에서 파열이
+            감지되었습니다.
+          </>
+        ) : (
+          <>
+            총 {analysisCount}장의 이미지를 분석한 결과, 파열이 확인되지
+            않았습니다.
+          </>
+        )}
       </div>
       {analysisItems.map((item: Sonography, index: number) => (
         <AnalysisResult key={item.id} index={index + 1} item={item} />
