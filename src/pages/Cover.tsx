0155706523ea6a4d71dd529e30d4eea65e825153
김기빈 <kibin3846@gmail.com>
2025-08-19 17:20:56 +0900

    feat: 커버에서 불필요 정보 삭제 및 ux writing

--- src/pages/Cover.tsx
+++ src/pages/Cover.tsx
@@ -7,24 +7,11 @@ const Cover = ({ hospitalName }: CoverProps) => {
     <div className="a4-page">
       <div className="a4-content">
         <div style={{ marginTop: "140px" }}>
-          <h1 className="cover-title-highlight">
-            AI 기반 초음파 분석을 활용한
-          </h1>
+          <h1 className="cover-title-highlight">AI를 활용한</h1>
           <h1 className="cover-title-normal">유방 보형물 진단</h1>
         </div>
-        <div style={{ marginTop: "20px" }}>
-          <p className="cover-description">
-            이 보고서는 AI 기반 진단 보조 시스템인
-          </p>
-          <div className="row align-center">
-            <img src="/images/wexpert.png" className="cover-wexpert-logo" />
-            <p className="cover-description cover-reposition">
-              를 통해 생성된 영상 분석 문서입니다.
-            </p>
-          </div>
-          <div style={{ marginTop: "50px" }}>
-            <h1 className="cover-hospital-name">{hospitalName}</h1>
-          </div>
+        <div style={{ marginTop: "50px" }}>
+          <h1 className="cover-hospital-name">{hospitalName}</h1>
         </div>
       </div>
       <div className="cover-footer">
