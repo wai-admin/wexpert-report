eeccd0fec5dd195cdbd1b906e35e8a9eb96df3b9
김기빈 <kibin3846@gmail.com>
2025-11-27 18:33:59 +0900

    feat: reportMode 수신 정보 변경

--- src/pages/ReportContainer.tsx
+++ src/pages/ReportContainer.tsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import { usePrintAction, useWebViewLoading } from "@/hooks";
+import { useMessageStore } from "@/store";
 import { PrintPage, ReportController } from "@/pages";
 import usePrintPageHandler from "@/hooks/usePrintPageHandler";
 import { ReportOptionType } from "@/lib";
@@ -7,6 +8,8 @@ import { ReportOptionType } from "@/lib";
 // WARNING: usePrintPageHandler 업데이트 시 전체 렌더링 주의 (개선 필요)
 const ReportContainer = () => {
   const printRef = useRef<HTMLDivElement>(null);
+  //Bridge Message
+  const { nativeMessage } = useMessageStore();
   // Data Information
   const { printPageData, option, isLoading } = usePrintPageHandler();
   // Handlers & State
@@ -21,13 +24,14 @@ const ReportContainer = () => {
   useWebViewLoading(isLoading);
 
   console.log("PrintPage Data Information: ", printPageData);
+  const reportMode = nativeMessage?.reportMode ?? ReportOptionType.NEW_REPORT;
 
   return (
     <div className="size-full flex justify-center relative">
       <div
         className={`h-full flex justify-center items-center
           ${
-            option.reportMode === ReportOptionType.ALL_REPORT_HISTORY
+            reportMode === ReportOptionType.ALL_REPORT_HISTORY
               ? "w-[calc(100%-var(--all-report-controller-width))] pr-[var(--all-report-controller-width)]"
               : "w-[calc(100%-var(--patient-report-controller-width))] pr-[var(--patient-report-controller-width)]"
           }
@@ -42,7 +46,7 @@ const ReportContainer = () => {
       <div className="fixed right-0 top-0">
         <ReportController
           printPageData={printPageData}
-          reportMode={option.reportMode}
+          reportMode={reportMode}
           onPrint={handlePrint}
         />
       </div>
