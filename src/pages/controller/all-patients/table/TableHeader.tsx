c2508a8a0c576f2833c7f143a8c26d1211fc4dfd
김기빈 <kibin3846@gmail.com>
2025-11-28 16:49:54 +0900

    feat: 정렬 조건에 따른 UI 처리

--- src/pages/controller/all-patients/table/TableHeader.tsx
+++ src/pages/controller/all-patients/table/TableHeader.tsx
@@ -1,19 +1,39 @@
 import { ALL_PATIENTS_TABLE_COLUMNS } from "@/constants";
+import { SortOrder, useAllPatientsFilterStore } from "@/store";
+
+// TODO: 추후 수정 필요
+const validSortKeys = ["report created", "name"];
 
 const TableHeader = () => {
+  const { sortBy, sortOrder } = useAllPatientsFilterStore();
+  const isSortedASC = sortOrder === SortOrder.ASC;
+
   return (
     <div className="w-full h-[48px] flex items-center bg-bg-base rounded-[6px] px-[14px]">
       {ALL_PATIENTS_TABLE_COLUMNS.map((column) => {
         const { key, title, width } = column;
+
+        const isValidSortKey =
+          validSortKeys.includes(key) &&
+          sortBy.toLowerCase() === key.toLowerCase();
+
         return (
           <div
             key={key}
-            className={`size-full flex items-center justify-center`}
+            className="size-full flex items-center justify-center gap-[5px]"
             style={{ maxWidth: `${width}px` }}
           >
             <p className="font-pretendard text-[16px] text-text-tertiary">
               {title}
             </p>
+            {isValidSortKey && (
+              <img
+                src="/images/arrow-sort-icon.png"
+                className={`w-[16px] h-[16px] ${
+                  isSortedASC ? "rotate-180" : ""
+                }`}
+              />
+            )}
           </div>
         );
       })}
