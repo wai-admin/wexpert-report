e435529d1e7d7e078225a9eb55f08770107022e9
김기빈 <kibin3846@gmail.com>
2025-11-30 20:33:47 +0900

    feat: 폰트 로드 속도 최적화

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import App from "./App.tsx";
 import "./i18n";
 import "./style/globals.css";
 import "./global.d.ts";
+import { registerServiceWorker } from "./utils/serviceWorker";
+
+// Service Worker 등록 (폰트 및 정적 자산 캐싱)
+if (import.meta.env.PROD) {
+  // 프로덕션 환경에서만 Service Worker 활성화
+  registerServiceWorker();
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
