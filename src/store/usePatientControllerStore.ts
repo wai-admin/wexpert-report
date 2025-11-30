4aee3bdda3c1b0642d0f9db48a5dfd25c48cec15
김기빈 <kibin3846@gmail.com>
2025-11-30 20:15:25 +0900

    fix: New Report로 탭 변경시 PrintPage가 표시되지 않는 이슈 해결

--- src/store/usePatientControllerStore.ts
+++ src/store/usePatientControllerStore.ts
@@ -1,5 +1,6 @@
 import { ReportTabValues } from "@/types";
 import { create } from "zustand";
+import { useReportListStore } from "./useReportList";
 
 interface PatientControllerState {
   selectedReportTab: ReportTabValues;
@@ -9,6 +10,13 @@ interface PatientControllerState {
 export const usePatientControllerStore = create<PatientControllerState>(
   (set) => ({
     selectedReportTab: ReportTabValues.NEW_REPORT,
-    setSelectedReportTab: (tab) => set({ selectedReportTab: tab }),
+    setSelectedReportTab: (tab) => {
+      set({ selectedReportTab: tab });
+
+      // NEW_REPORT 탭으로 변경 시 reportList를 비어있지 않음으로 설정
+      if (tab === ReportTabValues.NEW_REPORT) {
+        useReportListStore.getState().setIsReportListEmpty(false);
+      }
+    },
   })
 );
