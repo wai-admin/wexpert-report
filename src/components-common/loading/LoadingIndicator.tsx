59e73c45894fbd8e8dd20758d9977ee3a8a633ff
김기빈 <kibin3846@gmail.com>
2025-12-02 11:05:19 +0900

    style: LoadingIndicator 사이즈 업데이트

--- src/components-common/loading/LoadingIndicator.tsx
+++ src/components-common/loading/LoadingIndicator.tsx
@@ -27,9 +27,9 @@ export default LoadingIndicator;
 const LoadingSpinner = () => {
   return (
     <div className="size-full flex flex-col justify-center items-center">
-      <div className="relative size-10">
-        <div className="absolute w-full h-full border-[4px] border-component-blue rounded-full"></div>
-        <div className="absolute w-full h-full border-[4px] border-solid-lt-solid rounded-full animate-spin border-t-transparent"></div>
+      <div className="relative size-5">
+        <div className="absolute w-full h-full border-[3px] border-component-blue rounded-full"></div>
+        <div className="absolute w-full h-full border-[3px] border-solid-lt-solid rounded-full animate-spin border-t-transparent"></div>
       </div>
     </div>
   );
